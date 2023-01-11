a680a8fb91892dc0cacbeb8051ce0401c35d2efa
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-01-11 12:37:30 -0500

    Worked with website navigation

--- script.js
+++ script.js
@@ -107,3 +107,21 @@ document.querySelector('#container-book-list').addEventListener('click', (e) => 
   store.removeBook(id);
   UI.hideOrRemoveFieldet();
 });
+
+document.querySelector('#book-list-menu').addEventListener('click', () => {
+  document.querySelector('.books-container').classList.remove('hide');
+  document.querySelector('.form').classList.add('hide');
+  document.querySelector('.section-contact-info').classList.add('hide');
+});
+
+document.querySelector('#add-new-book-menu').addEventListener('click', () => {
+  document.querySelector('.form').classList.remove('hide');
+  document.querySelector('.books-container').classList.add('hide');
+  document.querySelector('.section-contact-info').classList.add('hide');
+});
+
+document.querySelector('#contact-menu').addEventListener('click', () => {
+  document.querySelector('.section-contact-info').classList.remove('hide');
+  document.querySelector('.form').classList.add('hide');
+  document.querySelector('.books-container').classList.add('hide');
+});
